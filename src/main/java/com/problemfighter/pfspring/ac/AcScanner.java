e44c5c13b0c1569230682e9bae06ae6ec6a1fb2b
hmtmcse <hmtm.cse@gmail.com>
2021-01-10 22:53:36 +0600

    bismillah

---
+++ src/main/java/com/problemfighter/pfspring/ac/AcScanner.java
@@ -0,0 +1,8 @@
+package com.problemfighter.pfspring.ac;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = {"com.problemfighter.pfspring.ac.*"})
+public class AcScanner {
+}
